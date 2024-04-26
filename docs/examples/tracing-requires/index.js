7cb1ce34d19ed8816e15094b3ef4fd03bb157c76
Darshan Sen <raisinten@gmail.com>
2024-04-26 12:13:57 +0530

    Add example for tracing `require()`s
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

---
+++ docs/examples/tracing-requires/index.js
@@ -0,0 +1,29 @@
+const { TraceEvents, trackRequires } = require("../../..");
+const { writeFileSync } = require('fs');
+
+const traceEvents = new TraceEvents();
+
+process.on("beforeExit", () => {
+  const events = traceEvents.getEvents();
+  traceEvents.destroy();
+  writeFileSync("events.json", JSON.stringify(events));
+});
+
+trackRequires(true);
+
+// Express Hello world example
+// Refs: https://expressjs.com/en/starter/hello-world.html
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let server;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  server.close();
+});
+
+server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
